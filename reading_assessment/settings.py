f1a62679e62b87d0c8d038e15abc3d7445d27259
jangamanikanta <129255548+jangamanikanta@users.noreply.github.com>
2025-11-08 00:02:53 +0530

    Add basic_auth_middleware to MIDDLEWARE settings

--- reading_assessment/settings.py
+++ reading_assessment/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'reading_assessment.reading_assessment.middleware.password_protect.basic_auth_middleware',
     'reading_assessment.middleware.password_protect.basic_auth_middleware',
    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
